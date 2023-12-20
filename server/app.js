4214e490a9bfdea6896665c995a91cc1c23d6af8
crisncris0000 <christopherrivera384@gmail.com>
2023-12-20 14:33:22 -0500

    Did update will need to test it out

--- server/app.js
+++ server/app.js
@@ -41,7 +41,20 @@ app.post('/api/favorite-wrestler/new', (req, res) => {
 });
 
 app.put('/api/wrestler/update', (req, res) => {
+    const { wrestlerName, brand, finisher, userDescription } = req.body;
 
+    pool.query(`UPDATE FavoriteWrestler wrestler_name = $1, brand = $2, wrestler_finisher = $3, user_description = $4`
+    [wrestlerName, brand, finisher, userDescription],
+    (error, result) => {
+        if(error) {
+            if(error) {
+                console.log(error);
+                res.sendStatus(500);
+            } else {
+                res.status(204).json("Updated");
+            }
+        }
+    })
 })
 
 app.listen(port, () => {
