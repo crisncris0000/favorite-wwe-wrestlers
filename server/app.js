2a06cfc799dae8c6d9d965d71e5227c3a3483cf3
crisncris0000 <christopherrivera384@gmail.com>
2023-12-20 13:51:19 -0500

    added a get request and changed pool connect

--- server/app.js
+++ server/app.js
@@ -3,29 +3,36 @@ const app = express();
 const port = 8080;
 
 const { Pool } = require('pg');
-const pool = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'favorite-wrestlersAPI',
+    password: 'root',
+    port: 5472,
+});
 
 app.use(express.json());
 
-pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'favlinksAPI',
-    password: 'root',
-    port: 5472,
-})
+app.get('/api/favorite-wrestlers', (req, res) => {
+    pool.query('SELECT * FROM FavoriteWrestler', (error, result) => {
+        if (error) {
+            console.log(error);
+            res.sendStatus(500);
+        } else {
+            res.status(200).json(result.rows);
+        }
+    });
+});
 
 app.post('/api/wrestler/new', (req, res) => {
     const { wrestlerName, brand, finisher, userDescription, firstName, lastName } = req.body;
 
     console.log(wrestlerName, brand, finisher, userDescription, firstName, lastName);
 
-
-
-    res.sendStatus(200)
+    res.sendStatus(200);
 });
 
-
 app.listen(port, () => {
-    console.log(`Server running on port:${port}`)
-})
+    console.log(`Server running on port:${port}`);
+});
--- server/script/script.sql
+++ server/script/script.sql
@@ -1,11 +1,13 @@
-CREATE DATABASE FavoriteWrestlersAPI;
+CREATE DATABASE "favorite-wrestlersAPI";
 
-\c FavoriteWrestlerAPI;
+\c favorite-wrestlersAPI;
 
+DROP TABLE IF EXISTS FavoriteWrestler;
 
 CREATE TABLE FavoriteWrestler(
     id SERIAL PRIMARY KEY,
     wrestler_name VARCHAR(100) NOT NULL,
+    brand VARCHAR(100) NOT NULL,
     wrestler_finisher VARCHAR(100) NOT NULL,
     user_description TEXT NOT NULL,
     first_name VARCHAR(255) NOT NULL,
