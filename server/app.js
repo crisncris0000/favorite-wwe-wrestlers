69d557bff24cbce57131661b2627e943d5fd1dac
crisncris0000 <christopherrivera384@gmail.com>
2023-12-20 11:28:22 -0500

    successfully tested the api endpoint with postman and will start connecting to a dataabase next

--- server/app.js
+++ server/app.js
@@ -2,6 +2,28 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+const { Pool } = require('pg');
+const pool = require('pg');
+
+app.use(express.json());
+
+pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'favlinksAPI',
+    password: 'root',
+    port: 5472,
+})
+
+app.post('/api/wrestler/new', (req, res) => {
+    const { wrestlerName, brand, finisher, userDescription, firstName, lastName } = req.body;
+
+    console.log(wrestlerName, brand, finisher, userDescription, firstName, lastName);
+
+
+
+    res.sendStatus(200)
+});
 
 
 app.listen(port, () => {
